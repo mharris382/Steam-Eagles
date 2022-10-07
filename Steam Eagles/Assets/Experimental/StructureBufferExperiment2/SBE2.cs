3a820c1c391549d1a718723e46529ac794651b2c
Max <mharris382@gmail.com>
2022-10-06 22:30:54 -0400

    made a new structured buffer experiment

---
+++ Steam Eagles/Assets/Experimental/StructureBufferExperiment2/SBE2.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Experimental.ComputeShaderExperiment2;
+public class SBE2 : Experimenter
+{
+    protected override IEnumerable<(string, Action)> GetButtonActions()
+    {
+        yield break;
+    }
+    
+    protected override void Init()
+    {
+        
+    }
+}
