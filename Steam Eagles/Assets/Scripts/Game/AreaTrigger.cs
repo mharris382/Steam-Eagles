9b36241f7ff7dd5c099f4633237a714612314cc7
Max <mharris382@gmail.com>
2022-10-14 18:07:15 -0400

    puzzle stuff

---
+++ Steam Eagles/Assets/Scripts/Game/AreaTrigger.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using StateMachine;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+namespace Game
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class AreaTrigger : MonoBehaviour
+    {
+        public SharedTransform p1Transform;
+        public SharedTransform p2Transform;
+
+         public BoolReactiveProperty player1InArea;
+         public BoolReactiveProperty player2InArea;
+         
+         
+         
+        private bool p1InArea
+        {
+            get => player1InArea.Value;
+            set => player1InArea.Value = value;
+        }
+        
+        private bool p2InArea
+        {
+            get => player2InArea.Value;
+            set => player2InArea.Value = value;
+        }
+
+
+
+        public UnityEvent<bool> onBothPlayersInArea;
+
+
+        private void Awake()
+        {
+            
+            player1InArea.ZipLatest(player2InArea, (b1, b2) => b1 && b2)
+                .Subscribe(both => onBothPlayersInArea?.Invoke(both));
+        }
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            var attachedRigidbody = col.attachedRigidbody;
+            if (attachedRigidbody == null) return;
+            var attachedTransform = attachedRigidbody.transform;
+            p1InArea = attachedTransform == p1Transform.Value;
+            p2InArea = attachedTransform == p2Transform.Value;
+        }
+    }
+}
--- Steam Eagles/Assets/Scripts/Game/PuzzleManager.cs
+++ Steam Eagles/Assets/Scripts/Game/PuzzleManager.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using World;
 
 public class PuzzleManager : MonoBehaviour
 {
@@ -8,11 +10,38 @@ public class PuzzleManager : MonoBehaviour
 }
 
 
-[CreateAssetMenu(menuName = "Game/Puzzle Data", fileName = "New Puzzle")]
-public class Puzzle : ScriptableObject
+public abstract class Puzzle : MonoBehaviour
 {
+    
     [SerializeField, Multiline(5)]
     public string description;
+
+
+    private int _totalAttempts;
     
+    /// <summary>number of times the puzzle has been restarted </summary>
+    public int TotalAttempts => _totalAttempts;
+
+
+    public abstract bool IsSolved
+    {
+        get;
+    }
+
+    /// <summary>
+    /// called the first time the puzzle is started by players
+    /// </summary>
+    public abstract void PuzzleStartup();
+    
+
+    protected abstract void StartPuzzle();
+    protected abstract void StopPuzzle(PuzzleResult result);
+    
+    public enum PuzzleResult
+    {
+        COMPLETED,
+        CANCELED,
+        FAILED
+    }
     
 }
