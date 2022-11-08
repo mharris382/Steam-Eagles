0fae147779e9b663a238023253427b0ab7652a6e
Max <mharris382@gmail.com>
2022-11-07 20:03:30 -0500

    Iteration 53 - Players Joining Game

--- Steam Eagles/Assets/Scripts/Characters/Input/PlayerCharacterManager.cs
+++ Steam Eagles/Assets/Scripts/Characters/Input/PlayerCharacterManager.cs
@@ -6,6 +6,7 @@ using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Object = UnityEngine.Object;
 
 namespace Characters
 {
@@ -13,105 +14,32 @@ namespace Characters
     [RequireComponent(typeof(PlayerInputManager))]
     public class PlayerCharacterManager : MonoBehaviour
     {
+
+        
+
+        
+        
         [SerializeField] List<CharacterAssignment> characterAssignments;
-
-        [Serializable]
-        public class CharacterAssignment
-        {
-            public string characterName;
-            [SerializeField] CharacterState prefab;
-            [Tooltip("This variable is required to support the dynamic split-screen feature")]
-            public SharedCamera playerCamera;
-            
-            [Header("Spawn Position")]
-            public SharedTransform spawnPoint;
-            public Vector3 defaultPosition;
-            
-            [Header("Debugging")]
-            public Color characterColor = Color.red;
-            public bool hideTransformInEditor = false;
-            public Vector3 SpawnPosition => SpawnTransform.position;
-            
-            /// <summary>
-            /// used to move the character's spawn point in the world
-            /// </summary>
-            public Transform SpawnTransform
-            {
-                get
-                {
-                    if (spawnPoint == null)
-                    {
-                        spawnPoint = ScriptableObject.CreateInstance<SharedTransform>();
-                        
-                    }
-
-                    if (!spawnPoint.HasValue)
-                    {
-                        spawnPoint.Value = new GameObject($"{characterName} Spawn Transform").transform;
-                        if(hideTransformInEditor)spawnPoint.Value.hideFlags = HideFlags.HideInHierarchy;
-                        spawnPoint.Value.position = defaultPosition;
-                        spawnPoint.name = $"{characterName} Spawn Transform";
-                    }
-                    return spawnPoint.Value;
-                }
-            }
-
-            void ResetSpawnPosition()
-            {
-                SpawnTransform.position = defaultPosition;
-            }
-            
-            
-            public CharacterInputState InstantiateCharacter()
-            {
-                var character = Instantiate(prefab, SpawnPosition, Quaternion.identity);
-                character.name = characterName;
-                CharacterInputState characterInputState;
-                if (!character.gameObject.TryGetComponent(out characterInputState))
-                {
-                    characterInputState = character.gameObject.AddComponent<CharacterInputState>();
-                }
-                
-                return characterInputState;
-            }
-            public void DestroyCharacter(CharacterState character)
-            {
-                Destroy(character.gameObject);
-            }
-            public void MoveCharacterToSpawn(CharacterState character)
-            {
-                character.transform.position = SpawnPosition;
-            }
-            
-
-            public void OnDrawGizmos()
-            {
-                Gizmos.color = characterColor;
-                Gizmos.DrawSphere(defaultPosition, 0.125f);
-            }
-        }
-
         
+        private PlayerInputManager _inputManager;
         private LinkedList<CharacterInputState> _unassignedCharacters = new LinkedList<CharacterInputState>();
-        private Queue<PlayerCharacterInput> _unassignedPlayers = new Queue<PlayerCharacterInput>();
-        
         private Dictionary<PlayerCharacterInput, CharacterInputState> _joinedPlayers = new Dictionary<PlayerCharacterInput, CharacterInputState>();
         private Dictionary<CharacterInputState, PlayerCharacterInput> _assignedCharacters = new Dictionary<CharacterInputState, PlayerCharacterInput>();
 
-        private PlayerInputManager _inputManager;
+       
+        [Obsolete("this can be removed, instead use the playerInputManager's limit number of players")]
+        private Queue<PlayerCharacterInput> _unassignedPlayers = new Queue<PlayerCharacterInput>(); //TODO: this can be removed, instead use the playerInputManager's limit number of players
         
-        bool AllCharacterAssigned => _unassignedCharacters.Count == 0;
-
-        private bool IsPlayerSpectating(PlayerCharacterInput playerCharacterInput)
-        {
-            return _unassignedPlayers.Contains(playerCharacterInput);
-        }
+        
+     
         
         private void Awake()
         {
             _inputManager = GetComponent<PlayerInputManager>();
+            
             _inputManager.onPlayerJoined += OnPlayerJoined;
             _inputManager.onPlayerLeft += OnPlayerLeft;
+            
             foreach (var characterAssignment in characterAssignments)
             {
                 var character = characterAssignment.InstantiateCharacter();
@@ -122,21 +50,7 @@ namespace Characters
                 Enqueue(character);
             }
         }
-
-        void OnCharacterDestroyed((CharacterInputState obj, CharacterAssignment prefab) destroyedCharacter)
-        {
-            //check if assigned character exists in dictionary
-            if(!_assignedCharacters.ContainsKey(destroyedCharacter.obj)) return;
-            var player = _assignedCharacters[destroyedCharacter.obj];
-            UnAssignCharacterToPlayer(player);
-            
-            _assignedCharacters.Remove(destroyedCharacter.obj);
-            
-            _joinedPlayers[player] = destroyedCharacter.prefab.InstantiateCharacter();
-            _assignedCharacters.Add(_joinedPlayers[player], player);
-            
-        }
-
+        
         private void OnPlayerLeft(PlayerInput obj)
         {
             var characterInput = GetCharacterInput(obj);
@@ -171,7 +85,7 @@ namespace Characters
             AssignCharacterToPlayer(characterInput);
         }
 
-        void AssignCharacterToPlayer(PlayerCharacterInput playerCharacterInput, CharacterInputState characterInputState = null)
+        private void AssignCharacterToPlayer(PlayerCharacterInput playerCharacterInput, CharacterInputState characterInputState = null)
         {
             //if there are no more characters to assign, wait for a character to be unassigned
             if (_unassignedCharacters.Count == 0)
@@ -187,19 +101,7 @@ namespace Characters
             Debug.Log($"Assigned {characterInputState.name} to Player #{playerCharacterInput.PlayerInput.playerIndex}");
         }
         
-        CharacterInputState Dequeue()
-        {
-            var ret = _unassignedCharacters.First.Value;
-            _unassignedCharacters.RemoveFirst();
-            return ret;
-        }
-        
-        void Enqueue(CharacterInputState characterInputState)
-        {
-            _unassignedCharacters.AddLast(characterInputState);
-        }
-
-        void UnAssignCharacterToPlayer(PlayerCharacterInput playerCharacterInput)
+        private void UnAssignCharacterToPlayer(PlayerCharacterInput playerCharacterInput)
         {
             if (!_joinedPlayers.ContainsKey(playerCharacterInput)) return;
             
@@ -214,9 +116,85 @@ namespace Characters
             
             Debug.Log($"Unassigned Character {characterInputState.name} from Player #{playerCharacterInput.PlayerInput.playerIndex}");
         }
+
+        
+
+        /// <summary>
+        /// respawns and reassigns a character if the character GameObject is destroyed for some reason.  the character
+        /// </summary>
+        /// <param name="destroyedCharacter"></param>
+        private void OnCharacterDestroyed((CharacterInputState obj, CharacterAssignment prefab) destroyedCharacter)
+        {
+            //check if assigned character exists in dictionary
+            if(!_assignedCharacters.ContainsKey(destroyedCharacter.obj)) return;
+            var player = _assignedCharacters[destroyedCharacter.obj];
+            UnAssignCharacterToPlayer(player);
+            
+            _assignedCharacters.Remove(destroyedCharacter.obj);
+            
+            _joinedPlayers[player] = destroyedCharacter.prefab.InstantiateCharacter();
+            _assignedCharacters.Add(_joinedPlayers[player], player);
+            
+        }
+
+        
+        #region [Helper Methods]
+
+
+        /// <summary>
+        /// gets PlayerCharacterInput component if exists, otherwise adds it
+        /// </summary>
+        /// <param name="playerInput"></param>
+        /// <returns></returns>
+        public PlayerCharacterInput GetCharacterInput(PlayerInput playerInput)
+        {
+            PlayerCharacterInput characterInput;
+            if (!playerInput.gameObject.TryGetComponent(out characterInput))
+            {
+                characterInput = playerInput.gameObject.AddComponent<PlayerCharacterInput>();
+            }
+
+            return characterInput;
+        }
+
+        [Obsolete("Should never have spectating players")]
+        private bool IsPlayerSpectating(PlayerCharacterInput playerCharacterInput)
+        {
+            return _unassignedPlayers.Contains(playerCharacterInput);
+        }
         
         
-        void ChangeAssignment(PlayerCharacterInput playerCharacterInput, CharacterAssignment newCharacterAssignment)
+        private  CharacterInputState Dequeue()
+        {
+            var ret = _unassignedCharacters.First.Value;
+            _unassignedCharacters.RemoveFirst();
+            return ret;
+        }
+        
+        private void Enqueue(CharacterInputState characterInputState)
+        {
+            _unassignedCharacters.AddLast(characterInputState);
+        }
+
+        
+
+        #endregion
+   
+        #region [Editor Gizmos]
+
+        public void OnDrawGizmos()
+        {
+            foreach (var characterAssignment in characterAssignments)
+            {
+                characterAssignment.OnDrawGizmos();
+            }
+        }
+        
+
+        #endregion
+        
+           
+        private void ChangeAssignment(PlayerCharacterInput playerCharacterInput, CharacterAssignment newCharacterAssignment)
         {
             if (!_joinedPlayers.ContainsKey(playerCharacterInput)) return;
             
@@ -231,25 +209,90 @@ namespace Characters
             
             Debug.Log($"Changed Character {characterInputState.name} to {newCharacterAssignment.characterName} for Player #{playerCharacterInput.PlayerInput.playerIndex}");
         }
-        
-        public PlayerCharacterInput GetCharacterInput(PlayerInput playerInput)
+
+    }   
+    
+    
+        [Serializable]
+        public class CharacterAssignment
         {
-            PlayerCharacterInput characterInput;
-            if (!playerInput.gameObject.TryGetComponent(out characterInput))
+            public string characterName;
+            [SerializeField] CharacterState prefab;
+            [Tooltip("This variable is required to support the dynamic split-screen feature")]
+            public SharedCamera playerCamera;
+            
+            [Header("Spawn Position")]
+            public SharedTransform spawnPoint;
+            public Vector3 defaultPosition;
+            
+            [Header("Debugging")]
+            public Color characterColor = Color.red;
+            public bool hideTransformInEditor = false;
+            
+            
+            
+            private CharacterInputState _spawnedCharacter;
+            
+            
+            
+            public Vector3 SpawnPosition => SpawnTransform.position;
+
+            
+            
+            
+            /// <summary>
+            /// used to move the character's spawn point in the world
+            /// </summary>
+            public Transform SpawnTransform
             {
-                characterInput = playerInput.gameObject.AddComponent<PlayerCharacterInput>();
+                get
+                {
+                    if (spawnPoint == null) spawnPoint = ScriptableObject.CreateInstance<SharedTransform>();
+                    if (!spawnPoint.HasValue)
+                    {
+                        spawnPoint.Value = new GameObject($"{characterName} Spawn Transform").transform;
+                        if(hideTransformInEditor)spawnPoint.Value.hideFlags = HideFlags.HideInHierarchy;
+                        spawnPoint.Value.position = defaultPosition;
+                        spawnPoint.name = $"{characterName} Spawn Transform";
+                    }
+                    return spawnPoint.Value;
+                }
             }
 
-            return characterInput;
-        }
-        
-        
-        public void OnDrawGizmos()
-        {
-            foreach (var characterAssignment in characterAssignments)
+            public CharacterInputState InstantiateCharacter()
             {
-                characterAssignment.OnDrawGizmos();
+                if (_spawnedCharacter != null) return _spawnedCharacter;
+                var character = Object.Instantiate(prefab, SpawnPosition, Quaternion.identity);
+                character.name = characterName;
+                CharacterInputState characterInputState;
+                if (!character.gameObject.TryGetComponent(out characterInputState))
+                {
+                    characterInputState = character.gameObject.AddComponent<CharacterInputState>();
+                }
+                _spawnedCharacter = characterInputState;
+                return characterInputState;
+            }
+        
+            
+            public void DestroyCharacter(CharacterState character)
+            {
+                Object.Destroy(character.gameObject);
+            }
+            
+            public void MoveCharacterToSpawn(CharacterState character)
+            {
+                character.transform.position = SpawnPosition;
+            }
+            
+            public void ResetSpawnPosition()
+            {
+                SpawnTransform.position = defaultPosition;
+            }
+            
+            public void OnDrawGizmos()
+            {
+                Gizmos.color = characterColor;
+                Gizmos.DrawSphere(defaultPosition, 0.125f);
             }
         }
-    }   
 }
