678fc590b5b9b37a9f8c2ae2f96b6e93bbb50416
Max <mharris382@gmail.com>
2022-11-08 13:50:04 -0500

    added a transform proxy component to help cinemachine target group

---
+++ Steam Eagles/Assets/Scripts/CoreLib/TransformProxy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace CoreLib
+{
+    public class TransformProxy : MonoBehaviour
+    {
+        private Transform _target;
+        public Transform Target
+        {
+            get => _target == null ? transform : _target;
+            set
+            {
+                _target = value;
+                enabled = _target != null;
+            }
+        }
+
+        private void Update()
+        {
+            transform.position = Target.position;
+        }
+    }
+}
