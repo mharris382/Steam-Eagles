cba4023d903f8a42bf0a02395b32630d09112740
Max <mharris382@gmail.com>
2022-11-08 01:14:38 -0500

    updated gas tank

---
+++ Steam Eagles/Assets/Scripts/Puzzles/GasTankState.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Puzzles
+{
+    [RequireComponent(typeof(HoldableItem))]
+    public class GasTankState : MonoBehaviour
+    {
+        public bool IsConnected
+        {
+            get;
+            set;
+        }
+
+        public BoolReactiveProperty isConnected;
+            public Events events;
+            [Serializable]
+            public class Events
+            {
+                public UnityEvent onBecameConnected;
+                public UnityEvent onBecameDisconnected;
+            }
+
+            public bool debugConnected;
+            private void Awake()
+            {
+                isConnected.TakeUntilDestroy(this).Subscribe(isConnected =>
+                {
+                    if (isConnected)
+                    {
+                        events.onBecameConnected?.Invoke();
+                    }
+                    else
+                    {
+                        events.onBecameDisconnected?.Invoke();
+                    }
+                });
+            }
+
+
+            private void Update()
+            {
+                if (debugConnected)
+                {
+                    isConnected.Value = !isConnected.Value;
+                    debugConnected = false;
+                }
+            }
+    }
+}
---
+++ Steam Eagles/Assets/Scripts/Puzzles/HoldableItem.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Puzzles
+{
+    public class HoldableItem : MonoBehaviour
+    {
+        public UnityEvent onPickedUp;
+        public UnityEvent onDropped;
+        
+        
+        public bool IsHeld
+        {
+            get;
+            set;
+        }
+    }
+}
