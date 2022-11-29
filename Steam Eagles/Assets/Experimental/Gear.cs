7d97e4b91e5108e14e84d00923f02454d9992e76
Max <mharris382@gmail.com>
2022-11-29 01:12:55 -0500

    Finally made a prototype gear system

--- Steam Eagles/Assets/Experimental/Gear.cs
+++ Steam Eagles/Assets/Experimental/Gear.cs
@@ -6,197 +6,65 @@ using Random = UnityEngine.Random;
 
 namespace Experimental
 {
+    [RequireComponent(typeof(CircleCollider2D))]
+    [RequireComponent(typeof(Rigidbody2D))]
     public class Gear : MonoBehaviour
     {
         public Gear parentGear;
-        [HideInInspector]
-        public List<Gear> childGear;
-
-        
-        public float radius = 2;
         public bool axelConnection = false;
         public float connectAngle = 15;
-        public bool IsRoot()
-        {
-            return GetComponent<Gears>() != null;
-        }
+        public bool IsRoot() => System != null && System.rootGear == this;
 
         public Vector3 Center => transform.position;
-
-
-        public Vector3 GetChildPosition(float childRadius, float childAttachAngle)
-        {
-            Vector3 dir =  Quaternion.Euler(0, 0, childAttachAngle) * Vector3.right;
-            var offset = dir * (radius + childRadius);
-            return Center + offset;
-        }
-
-       
-    }
-
-
-
-
-#if UNITY_EDITOR
-    [CustomEditor(typeof(Gear))]
-    public class GearEditor : Editor
-    {
-        Gear Gear => target as Gear;
-
         
 
-        public float StartRotationAngle
+        public List<Gear> childGears;
+
+        public Gears System
         {
-            get => Gear.transform.eulerAngles.z;
+            get => _system;
+            set => _system = value;
+        }
+
+        public Rigidbody2D Rb => _rb ? _rb : _rb = GetComponent<Rigidbody2D>();
+
+        public CircleCollider2D CircleCollider2D =>
+            _circleCollider2D ? _circleCollider2D : _circleCollider2D = GetComponent<CircleCollider2D>();
+
+        public float AngularVelocity
+        {
             set
             {
-                
-                UpdateStartRotation(value);
-            }
-        }
-        
-        
-
-        private void UpdateStartRotation(float value)
-        {
-            var current = Gear;
-            float curRotation = value;
-            current.transform.rotation = Quaternion.Euler(0, 0, value);
-            while (current.parentGear != null)
-            {
-                UpdateParent(current, curRotation);
-            }
-            UpdateChildren(Gear, value);
-        }
-
-        private void UpdateChildren(Gear parent, float newParentRotation)
-        {
-            var childRotation = -newParentRotation;
-            parent.transform.rotation = Quaternion.Euler(0, 0, childRotation);
-            foreach (var gear in parent.childGear)
-            {
-                if (gear.axelConnection) continue;
-                UpdateChildren(gear, childRotation);
-            }
-        }
-
-        public Gear UpdateParent(Gear child, float childRotation)
-        {
-            if (child.IsRoot()) return null;
-            var newParentRotation = -childRotation;
-            child.parentGear.transform.rotation = Quaternion.Euler(0, 0, newParentRotation);
-            return child.parentGear;
-        }
-        
-        public override void OnInspectorGUI()
-        {
-            if (Application.isPlaying==false)
-            {
-                EditorGUI.BeginChangeCheck();
-                float rotation = EditorGUILayout.FloatField("Current Rotation", StartRotationAngle);
-                if (EditorGUI.EndChangeCheck())
+                Rb.angularVelocity = value;
+                foreach (var childGear in childGears)
                 {
-                    StartRotationAngle = rotation;
+                    if (childGear == null) return;
+                    childGear.AngularVelocity = ComputeChildGearVelocity(value, childGear);
                 }
             }
-
-            if (GUILayout.Button("Create Child"))
-            {
-                var go = Instantiate(Gear, Gear.GetChildPosition(Gear.radius, Random.Range(0f, 360f)),
-                    Quaternion.identity);
-                go.transform.SetParent(Gear.transform.parent);
-                var childGear = go.GetComponent<Gear>();
-                Gear.childGear.Add(childGear);
-                childGear.parentGear = Gear;
-            }
-            base.OnInspectorGUI();
-
-            GUILayout.Space(5);
-            
-          
         }
 
-        void DrawHandles(Gear gear, bool findRoot =true)
+        public float Radius
         {
-            var root = gear;
-            if (findRoot)
-            {
-                while (root.parentGear != null)
-                {
-                    root = root.parentGear;
-                }
-            }
-            Handles.color = Color.Lerp(Color.blue, Color.cyan, 0.5f);
-            Handles.DrawWireDisc(root.transform.position, Vector3.forward, root.radius);
-            foreach (var gear1 in root.childGear)
-            {
-                var p0 = gear1.Center;
-                var p1 = gear.Center;
-                Handles.DrawDottedLine(p0, p1, HandleUtility.GetHandleSize(p1) * 0.3f);
-                DrawHandles(gear1, false);
-            }
-            /*
-            Handles.DrawWireDisc(gear.transform.position, Vector3.forward, gear.radius);
-            DrawHandlesParents(gear);
-            
-            foreach (var gear1 in gear.childGear)
-            {
-                var p0 = gear1.Center;
-                var p1 = gear.Center;
-                Handles.DrawDottedLine(p0, p1, HandleUtility.GetHandleSize(p1) * 0.3f);
-                DrawHandles(gear1);
-            }*/
+            get => CircleCollider2D.radius;
         }
 
-        void DrawHandlesParents(Gear gear)
+        private float ComputeChildGearVelocity(float value, Gear childGear)
         {
-            if (gear.parentGear != null)
-            {
-                gear = gear.parentGear;
-                Handles.color = Color.Lerp(Color.blue, Color.green, 0.5f);
-                Handles.DrawWireDisc(gear.transform.position, Vector3.forward, gear.radius);
-                DrawHandlesParents(gear);
-            }
-            
-            foreach (var gear1 in gear.childGear)
-            {
-                var p0 = gear1.Center;
-                var p1 = gear.Center;
-                Handles.DrawDottedLine(p0, p1, HandleUtility.GetHandleSize(p1) * 0.3f);
-            }
+            return -value;
         }
-        private void OnSceneGUI()
-        {
-            var gear = target as Gear;
-            DrawHandles(Gear);
-            if (Gear.parentGear!=null)
-            {
-                UpdateChildPosition(Gear);
-            }
-        }
+        private CircleCollider2D _circleCollider2D;
+        private Rigidbody2D _rb;
+        private Gears _system;
 
-        private void UpdateChildPosition(Gear gear)
+        private void Awake()
         {
-            if (gear.parentGear == null)
-            {
-                return;
-            }
-
-            if (gear.axelConnection)
-            {
-                gear.transform.position = gear.parentGear.Center;
-            }
-            else
-            {
-                var gearPos = gear.parentGear.GetChildPosition(gear.radius, gear.connectAngle);
-                gear.transform.position = gearPos;
-            }
-            foreach (var gear1 in gear.childGear)
-            {
-                UpdateChildPosition(gear1);
-            }
+            _system = GetComponent<Gears>();
+            childGears.Clear();
+            tag = "Gear";
         }
     }
-    
-#endif
+
+
+
 }
--- Steam Eagles/Assets/Experimental/Gears.cs
+++ Steam Eagles/Assets/Experimental/Gears.cs
@@ -1,9 +1,81 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace Experimental
 {
     public class Gears : MonoBehaviour
     {
         public Gear rootGear;
+        public LayerMask gearLayers;
+        public float bufferRadius = 0.2f;
+        public float gearRPM = 6;
+
+        private float _currentRpm;
+        private float _rpmT;
+        
+        public List<Gear> gearsInSystem;
+        private void Start()
+        {
+            rootGear = GetComponent<Gear>();
+            gearsInSystem.Clear();
+            SolveGearChain();
+        }
+
+        public void SolveGearChain()
+        {
+            
+            HashSet<Gear> searchedGears = new HashSet<Gear>();
+            Stack<Gear> unsearchedGears = new Stack<Gear>();
+            
+            unsearchedGears.Push(rootGear);
+            
+            while (unsearchedGears.Count > 0)
+            {
+                var currentGear = unsearchedGears.Pop();
+                
+                if (searchedGears.Contains(currentGear))
+                    continue;
+                
+                searchedGears.Add(currentGear);
+                foreach (var gear in  Physics2D.OverlapCircleAll(currentGear.Center, currentGear.Radius+bufferRadius, gearLayers).Select(t => t.GetComponent<Gear>()))
+                {
+                    if (gear == null)
+                        continue;
+                    if (searchedGears.Contains(gear)) continue;
+                    if (gear.System != null && gear.System != this)
+                    {
+                        Debug.LogError("Gear Systems overlapping!", gear.System);
+                        return;
+                    }
+
+                    if (gear == currentGear.parentGear) continue;
+                    if (gear == currentGear) continue;
+
+                    gear.parentGear = currentGear;
+                    
+                    currentGear.childGears.Add(gear);
+                    gear.System = this;
+                    unsearchedGears.Push(gear);
+                }
+                
+            }
+
+            gearsInSystem = searchedGears.ToList();
+        }
+
+        private void Update()
+        {
+            MoveGearChain();
+        }
+
+        public void MoveGearChain()
+        {
+           _currentRpm = Mathf.SmoothDamp(_currentRpm, gearRPM, ref _rpmT, Time.deltaTime);
+           float radPerSec = _currentRpm * 0.10472f;
+           float degPerSec = radPerSec * Mathf.Rad2Deg;
+           rootGear.AngularVelocity = degPerSec;
+        }
     }
 }
