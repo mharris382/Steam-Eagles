9a227a99533aa418f3b29fc4cb277f88f78a2b2f
Max <mharris382@gmail.com>
2022-11-29 20:08:32 -0500

    started on a generic gun 2d, just for fun

--- Steam Eagles/Assets/Scripts/CoreLib/Input/GunSight2D.cs
+++ Steam Eagles/Assets/Scripts/CoreLib/Input/GunSight2D.cs
@@ -2,6 +2,7 @@
 
 namespace Utilities
 {
+    [ExecuteAlways]
     public class GunSight2D : MonoBehaviour
     {
         public Gun2D gun2D;
@@ -10,6 +11,8 @@ namespace Utilities
         public LayerMask layerMask ;
         private void Update()
         {
+            if(gun2D == null || lineRenderer == null)
+                return;
             lineRenderer.SetPosition(0, gun2D.FirePoint.position);
             var hit = Physics2D.Raycast(gun2D.FirePoint.position, gun2D.FirePoint.right, 100, layerMask);
             if (hit)
--- Steam Eagles/Assets/Scripts/CoreLib/Input/ParticleGun2D.cs
+++ Steam Eagles/Assets/Scripts/CoreLib/Input/ParticleGun2D.cs
@@ -7,83 +7,83 @@ namespace Utilities
     [RequireComponent(typeof(Rigidbody2D))]
     public abstract class Gun2D : MonoBehaviour
     {
+        
         private Rigidbody2D _rb;
         public Rigidbody2D rb => _rb ? _rb : _rb = GetComponent<Rigidbody2D>();
-        
-        
-    }
 
-    public abstract class Gun2DInput : MonoBehaviour
-    {
-        
-        
-    }
-    
-    
-    
-    public class GunModule
-    {
-            
-    }
-    
-    [RequireComponent(typeof(Rigidbody2D))]
-    [RequireComponent(typeof(ParticleSystem))]
-    public class ParticleGun2D : MonoBehaviour
-    {
-        private ParticleSystem _ps;
-        public ParticleSystem ps => _ps ? _ps : _ps = GetComponent<ParticleSystem>();
-        
-        
-        
-        
-        
-        [Serializable]
-        public class GunShooter
+        public abstract Transform FirePoint { get; set; }
+
+        public float AimDirection
         {
-            public float timeBetweenShots = 0.5f;
-            
-            [Header("Debug Values")] [NonSerialized] 
-            public float timeListFired;
-            
-            public bool CanFire()
-            {
-                return Time.time - timeListFired > timeBetweenShots;
-            }
-            
-            public void Fire(ParticleGun2D particleGun)
-            {
-                timeListFired = Time.time;
-            }
+            get => rb.rotation;
+            set => rb.rotation = value;
         }
 
-        
-        [Serializable]
-        public class GunAim
+        public bool drawGizmos = true;
+        public Color gizmoGolor = Color.yellow;
+        private void OnDrawGizmos()
         {
-            
-            public float aimSpeed = 1f;
-            public float aimAngleOffset = 0f;
-            
-            
-        }
-        
-        
-        public void Shoot(float angle, float speed)
-        {
-            rb.rotation = angle;
-            var main = ps.main;
-            main.simulationSpeed = speed;
-            ps.Emit(1);
+            if(!drawGizmos)return;
         }
     }
 
-    public class ParticleGun2DTester : ParticleGun2D
-    {
-        public Camera camera;
-        
-        private void Update()
-        {
-                
-        }
-    }
+    //
+    //
+    // [RequireComponent(typeof(Rigidbody2D))]
+    // [RequireComponent(typeof(ParticleSystem))]
+    // public class ParticleGun2D : Gun2D
+    // {
+    //     private ParticleSystem _ps;
+    //     public ParticleSystem ps => _ps ? _ps : _ps = GetComponent<ParticleSystem>();
+    //     
+    //     
+    //     
+    //     
+    //     
+    //     [Serializable]
+    //     public class GunShooter
+    //     {
+    //         public float timeBetweenShots = 0.5f;
+    //         
+    //         [Header("Debug Values")] [NonSerialized] 
+    //         public float timeListFired;
+    //         
+    //         public bool CanFire()
+    //         {
+    //             return Time.time - timeListFired > timeBetweenShots;
+    //         }
+    //         
+    //         public void Fire(ParticleGun2D particleGun)
+    //         {
+    //             timeListFired = Time.time;
+    //         }
+    //     }
+    //
+    //     
+    //     [Serializable]
+    //     public class GunAim
+    //     {
+    //         
+    //         public float aimSpeed = 1f;
+    //         public float aimAngleOffset = 0f;
+    //         
+    //         
+    //     }
+    //     
+    //     
+    //     public void Shoot(float angle, float speed)
+    //     {
+    //         rb.rotation = angle;
+    //         var main = ps.main;
+    //         main.simulationSpeed = speed;
+    //         ps.Emit(1);
+    //     }
+    // }
+    //
+    // public class ParticleGun2DTester : ParticleGun2D
+    // {
+    //     public Camera camera;
+    //     
+    //     
+    // }
 }
