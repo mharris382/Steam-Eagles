7f930f1fc008ee2e537804b6bac2da2cf9174180
mAX <mharris382@gmail.com>
2022-12-05 20:06:50 -0500

    started work on new level

---
+++ Steam Eagles/Assets/Scripts/UI/UIBlockBuilding.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using CoreLib;
+//using Spaces;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using World;
+
+namespace UI
+{
+    public class UIBlockBuilding : MonoBehaviour
+    {
+        public string resourceTag;
+        public TargetingResources targetingResources;
+        private TilemapResources _tilemapResources;
+        
+        
+        public BlockSlot[] blockSlots;
+        public TilemapResources Tilemaps => _tilemapResources ?? new TilemapResources();
+        
+        
+        [Serializable]
+        public class BlockSlot
+        {
+            public string blockName;
+           public Tile tile;
+           public SharedInt blockCount;
+            [Range(-1, 100)]
+            public int startingBlockAmount = 5;
+
+            public bool IsValid() => tile != null && blockCount != null;
+
+
+            public void SetStartingBlockAmount()
+            {
+                if (startingBlockAmount == -1) return;
+                if(!IsValid()) Debug.LogError("Block slot");
+            }
+        }
+        
+        
+        public class TilemapResources
+        {
+            /// <summary>
+            /// returns all tilemaps which 
+            /// </summary>
+            /// <returns></returns>
+            public Tilemap[] GetAllBuildableTilemaps()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        
+        
+        [Serializable]
+        public class TargetingResources
+        {
+            public string resourceTag = "Builder";
+            [SerializeField] Camera camera;
+            [SerializeField] SharedCamera sharedCamera;
+           // [SerializeField] private bool findTaggedCamera = false;
+            [SerializeField] private bool respondToEvents = false;
+            [SerializeField] private bool log;
+            private Camera _camera;
+
+            void Log(string msg, UnityEngine.Object obj) { if (log) Debug.Log(msg, obj); }
+            
+            public Camera Camera
+            {
+                get
+                {
+                    if (_camera == null)
+                    {
+                        Camera FindTaggedCamera()
+                        {
+                            var anyCameras = FindObjectsOfType<Camera>();
+                            Camera taggedCamera = null;
+                            foreach (var anyCamera in anyCameras)
+                            {
+                                if (anyCamera.CompareTag(resourceTag))
+                                {
+                                    taggedCamera = anyCamera;
+                                    Debug.Log($"{resourceTag} - UI Block Targeting Found Tagged Camera {anyCamera.name}");
+                                    break;
+                                }
+                            }
+                            if (taggedCamera == null) Debug.LogWarning($"{resourceTag} was unable to locate a tagged camera!");
+                            return taggedCamera;
+                        }
+
+                        var taggedCamera = FindTaggedCamera();
+                       
+                        if (sharedCamera != null)
+                        {
+                            void AssignSharedCamera(Camera toAssign)
+                            {
+                                Debug.Assert(toAssign != null);
+                                _camera = camera = sharedCamera.Value = toAssign;
+                                Log($"{resourceTag} - Targeting assigned {sharedCamera.name}.Value to {camera}", camera);
+                            }
+                            if (taggedCamera != null)
+                                AssignSharedCamera(taggedCamera);
+                            if (camera != null)
+                                AssignSharedCamera(camera);
+                            else if (sharedCamera.HasValue)
+                                AssignSharedCamera(sharedCamera.Value);
+                            else
+                            {
+                                
+                                
+                            }
+                            if (Camera.main != null)
+                            {
+                                
+                            }
+                            else
+                            {
+                                Debug.LogError($"{resourceTag} - Targeting found no Main Camera in scene!");
+                               
+                                
+                                
+                            }
+                        }
+                        else if (camera == null)
+                        {
+                            _camera = camera = Camera.main;  
+                            Log($"{resourceTag} - Targeting is now hard coded to use main camera: {camera}", camera);
+                        }
+                        else
+                        {
+                            _camera = camera;
+                            Log($"{resourceTag} - Targeting is hard coded to use camera: {camera}", camera);
+                        }
+                    }
+                    Debug.Assert(_camera!=null, "Missing targeting camera!");
+                    return _camera;
+                }
+            }
+        }
+    }
+}
---
+++ Steam Eagles/Assets/Scripts/UI/UISelectorWheelAdvanced.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    using CoreLib;
+using DG.Tweening;
+using UniRx;
+using UnityEngine;
+
+namespace UI
+{
+    public class UISelectorWheel : MonoBehaviour
+    {
+        public SharedInt SelectedIndex;
+        [Min(2)]
+        public int numberOfItems = 2;
+        public UISelectedAbilitySlot slotPrefab;
+        public SharedInt selectedSlotIndex;
+        
+        [Range(-360, 360)]
+        public float offsetRotation;
+        [Header("Tween properties"), Range(0, 1)]
+        public float movementDuration = 0.50f;
+
+        public float min = 0;
+        public float max = 360;
+        public RotateMode rotateMode;
+        public Ease movementEase = Ease.Linear;
+        private int _selectedIndex;
+        public int selectedIndex
+        {
+            get => _selectedIndex;
+            set
+            {
+                _selectedIndex = value;
+                StartWheelMovement();
+            }
+        }
+
+        private float _previousTargetRotation;
+        private float _currentTargetRotation;
+
+        public bool testUp;
+        public bool testDown;
+        public int testUpNumber = 1;
+        
+        private void Start()
+        {
+            if (SelectedIndex != null)
+            {
+                SelectedIndex.Value = 0;
+                SelectedIndex.onValueChanged.AsObservable().TakeUntilDestroy(this).Subscribe(index => selectedIndex = index);
+            }
+            
+        }
+        public void Update()
+        {
+            if (testDown)
+            {
+                if (_selectedIndex == 0)
+                {
+                    selectedIndex = numberOfItems;
+                }
+                else
+                {
+                    selectedIndex--;
+                }
+
+                testDown = false;
+                StartWheelMovement();
+            }
+
+            if (testUp)
+            {
+                _selectedIndex+= testUpNumber % numberOfItems;
+                _selectedIndex %= numberOfItems;
+                testUp = false;
+                StartWheelMovement();
+            }
+        }
+
+        
+        
+        public void StartWheelMovement()
+        {
+            _currentTargetRotation = GetDesiredRotation(_selectedIndex, numberOfItems, offsetRotation, min, max);
+            var rt = this.GetComponent<RectTransform>();
+            rt.DOKill();
+            
+            rt.DORotateQuaternion(Quaternion.Euler(0, 0, _currentTargetRotation), movementDuration).SetEase(movementEase)
+                .Play().SetAutoKill(true);
+            
+                
+        }
+        
+        private static float GetDesiredRotation(int targetIndex, int count, float rotationOffset, float min = 0, float max = 360)
+        {
+            float t =  (max / (float) count);
+
+            float anglePerItemDeg = t;
+            
+            //float anglePerItemRad = anglePerItemDeg * Mathf.Deg2Rad;
+            return targetIndex * anglePerItemDeg + rotationOffset;
+        }
+        
+    }
+}
+}
