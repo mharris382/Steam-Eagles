b2a507ad915511a4ed47182fcd94dabc7e1ce6d6
Max <mharris382@gmail.com>
2022-12-20 03:30:23 -0500

    UNTESTED added wall fader scripts for various types of hidden areas

---
+++ Steam Eagles/Assets/Scripts/PhysicsFun/GenericWallFader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace PhysicsFun
+{
+    public class GenericWallFader : WallFaderBase
+    {
+        public Color wallColor = Color.white;
+        public UnityEvent<Color> onWallColorChange;
+        public UnityEvent<float> onWallAlphaChange;
+        public override void SetWallAlpha(float alpha)
+        {
+            // do something
+            var color = wallColor;
+            color.a = alpha;
+            onWallColorChange?.Invoke(color);
+            onWallAlphaChange?.Invoke(alpha);
+        }
+    }
+}
---
+++ Steam Eagles/Assets/Scripts/PhysicsFun/WallSpriteShapeFader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.U2D;
+
+namespace PhysicsFun
+{
+    [RequireComponent(typeof(SpriteShapeRenderer))]
+    public class WallSpriteShapeFader : WallFaderBase
+    {
+        public SpriteShapeRenderer spriteShapeController;
+        public override void SetWallAlpha(float alpha)
+        {
+            // do something
+            
+            spriteShapeController.color = new Color(1, 1, 1, alpha);
+        }
+    }
+}
