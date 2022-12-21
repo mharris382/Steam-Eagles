4566390ac5d8783d4c99fb1dfe48b9a67527dffa
Max <mharris382@gmail.com>
2022-12-20 22:18:18 -0500

    reusable water check script inspired by propeller

---
+++ Steam Eagles/Assets/Scripts/PhysicsFun/WaterCheck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace PhysicsFun
+{
+    [Serializable]
+    public class WaterCheck
+    {
+        [SerializeField] private float checkRadius = 0.5f;
+        [SerializeField] private BoolReactiveProperty isUnderWater = new BoolReactiveProperty(false);
+    
+        public IReadOnlyReactiveProperty<bool> IsUnderWater => isUnderWater;
+        public float Radius => checkRadius;
+        public bool IsSubmerged(Transform transform)
+        {
+            LayerMask mask = LayerMask.GetMask("Water");
+            var pos = transform.position;
+            var coll = Physics2D.OverlapCircle(pos, checkRadius, mask);
+            return coll != null;
+        }
+    }
+
+    
+}
