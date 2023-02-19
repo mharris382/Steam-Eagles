b08c7c1c8f3057db0ba7351328c6cae1f1692d06
Max <mharris382@gmail.com>
2023-02-18 19:38:57 -0500

    added lore info to the Items

--- Steam Eagles/Assets/Scripts/Items/ItemBase.cs
+++ Steam Eagles/Assets/Scripts/Items/ItemBase.cs
@@ -18,12 +18,20 @@ namespace Items
         [HorizontalGroup("Item Info", width:0.8f, LabelWidth = 54)]
         [VerticalGroup("Item Info/Name_Desc")]
         [Multiline(5)] public string description;
+        
+
 
         [VerticalGroup("Item Info/Name_Desc")] [HorizontalGroup("Item Info/Name_Desc/Dropping", width: 0.5f)]
         [ToggleLeft]
         public bool canDrop;
         [HorizontalGroup("Item Info/Name_Desc/Dropping", width: 0.7f), ShowIf(nameof(canDrop))]
         public string dropKey;
+        
+        
+        [FoldoutGroup("Lore",expanded:false)]
+        [Tooltip("a narrative description of the item, which may or may not directly pertain to gameplay but connects the item to the story and reveals more details about the world's lore")]
+        [Multiline(5),HideLabel] public string lore;
+        
         public virtual bool IsStackable => MaxStackSize > 1;
         public abstract int MaxStackSize { get; }
     }
--- Steam Eagles/Assets/Scripts/Items/UI/UIInventory.cs
+++ Steam Eagles/Assets/Scripts/Items/UI/UIInventory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using Sirenix.OdinInspector;
 using UI;
 using UniRx;
@@ -13,15 +14,25 @@ namespace Items.UI
         
         public Inventory inventory;
 
+        public string slotKey = "ItemSlot";
 
 
         private IDisposable _disposable;
 
+        IEnumerator WaitForSlotToBeReady()
+        {
+            while (!UIInventoryItemSlots.Instance.IsUIReady(slotKey))
+            {
+                yield return null;
+            }
+            Open();
+        }
         public override void Open()
         {
-            if (!UIInventoryItemSlots.Instance.IsReady)
+            if (!UIInventoryItemSlots.Instance.IsUIReady(slotKey))
             {
                 Close();
+                StartCoroutine(WaitForSlotToBeReady());
                 return;
             }
 
@@ -34,9 +45,9 @@ namespace Items.UI
             CompositeDisposable disposable = new CompositeDisposable();
             foreach (var inventoryItemSlot in inventory.itemSlots)
             {
-                var uiItemSlot = UIInventoryItemSlots.Instance.GetUIItemSlot(inventoryItemSlot);
+                var uiItemSlot = UIInventoryItemSlots.Instance.GetUIItem(slotKey, inventoryItemSlot);
                 uiItemSlot.transform.SetParent(uiItemParent);
-                disposable.Add(Disposable.Create(() => UIInventoryItemSlots.Instance.ReturnUIItemSlot(uiItemSlot)));
+                disposable.Add(Disposable.Create(() => UIInventoryItemSlots.Instance.ReturnUIItemSlot(slotKey, uiItemSlot)));
             }
 
             _disposable = disposable;
