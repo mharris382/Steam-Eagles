f10cf32edc0bbf2e040297e0c919bf9c4ce613d2
Max <mharris382@gmail.com>
2023-02-22 01:53:57 -0500

    UI mockup nonsense

---
+++ Steam Eagles/Assets/Scripts/UI/Core/UIGameManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CoreLib;
+using UnityEngine;
+using FSM;
+namespace UI.Core
+{
+    public class UIGameManager : Singleton<UIGameManager>
+    {
+        private FSM.StateMachine _rootStateMachine;
+
+
+        private void Awake()
+        {
+            _rootStateMachine = new FSM.StateMachine();
+        }
+    }
+}
---
+++ Steam Eagles/Assets/Scripts/UI/Core/UIState.cs
@@ -0,0 +1,7 @@
+﻿namespace UI.Core
+{
+    public abstract class UIState : FSM.State
+    {
+        
+    }
+}
