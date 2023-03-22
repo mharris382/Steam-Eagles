1134cab7556153da8f94a7424d53c4a9ceb1bcc5
Steph <sljeandell@gmail.com>
2023-03-22 18:35:46 -0400

    added new script to setup line renderers for joints

---
+++ Steam Eagles/Assets/Scripts/PhysicsFun/AutoJointRenderer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class AutoJointRenderer : MonoBehaviour
+{
+    
+    [Sirenix.OdinInspector.Required] public GameObject target;
+    [Sirenix.OdinInspector.Required,OnValueChanged(nameof(UpdateLrs))] public Material lineMaterial;
+    public bool createParent = true;
+
+    public float zOffset = 0;
+    [Range(0, 10),OnValueChanged(nameof(UpdateLrs))] public float width = 1;
+    [OnValueChanged(nameof(UpdateLrs))] public Color lineColor = Color.white;
+
+    [SerializeField, OnValueChanged(nameof(UpdateLrs))] private string sortingLayer = "Default";
+    [SerializeField, OnValueChanged(nameof(UpdateLrs))] private int orderInLayer;
+
+    private AnchoredJoint2D[] _joints;
+    private JointLineRenderer[] _jointsRenderers;
+    private void Awake()
+    {
+        _joints = target.GetComponents<AnchoredJoint2D>();
+        _jointsRenderers = new JointLineRenderer[_joints.Length];
+        var parent = gameObject;
+        if (createParent)
+        {
+            parent = new GameObject("JointRenderers");
+            parent.transform.SetParent(transform);
+        }
+        for (int i = 0; i < _joints.Length; i++)
+        {
+            var joint = _joints[i];
+            var go = new GameObject("JointLineRenderer " + i);
+            go.transform.SetParent(parent.transform);
+            var lr = go.AddComponent<LineRenderer>();
+            
+            UpdateRenderer(lr);
+            _jointsRenderers[i] = new JointLineRenderer(joint, lr);
+        }
+    }
+
+    void UpdateLrs()
+    {
+        if(_jointsRenderers == null) return;
+        foreach (var jointRenderer in _jointsRenderers)
+        {
+            UpdateRenderer(jointRenderer.lineRenderer);
+        }
+    }
+
+    private void UpdateRenderer(LineRenderer lr)
+    {
+        lr.material = lineMaterial;
+        lr.useWorldSpace = true;
+        lr.startWidth = lr.endWidth = width;
+        lr.textureMode = LineTextureMode.Tile;
+        lr.loop = false;
+        lr.startColor = lr.endColor = lineColor;
+        lr.sortingLayerName = sortingLayer;
+        lr.sortingOrder = orderInLayer;
+    }
+
+    private void Update()
+    {
+        foreach (var jointRenderer in _jointsRenderers)
+        {
+            jointRenderer.Update(zOffset);
+        }
+    }
+
+    private class JointLineRenderer
+    {
+        private readonly AnchoredJoint2D _joint2D;
+        public readonly LineRenderer lineRenderer;
+        
+        public JointLineRenderer(AnchoredJoint2D joint2D, LineRenderer lineRenderer)
+        {
+            _joint2D = joint2D;
+            this.lineRenderer = lineRenderer;
+        }
+
+        public void Update(float zOffset)
+        {
+            lineRenderer.positionCount = 2;
+            var connectedAnchor = _joint2D.connectedAnchor;
+            lineRenderer.SetPosition(0, _joint2D.connectedBody.transform.position + new Vector3(connectedAnchor.x, connectedAnchor.y, zOffset));
+            var anchor = _joint2D.anchor;
+            lineRenderer.SetPosition(1, _joint2D.transform.position + new Vector3(anchor.x, anchor.y, zOffset));
+        }
+    }
+}
--- Steam Eagles/Assets/Scripts/PhysicsFun/JointLineRenderer.cs
+++ Steam Eagles/Assets/Scripts/PhysicsFun/JointLineRenderer.cs
@@ -1,23 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
 [ExecuteAlways]
 public class JointLineRenderer : MonoBehaviour
 {
     private LineRenderer _lr;
-    private LineRenderer lr => _lr ? _lr : _lr = GetComponent<LineRenderer>();
+    internal LineRenderer lr => _lr ? _lr : _lr = GetComponent<LineRenderer>();
 
     [SerializeField]
-    private AnchoredJoint2D joint;
+    internal AnchoredJoint2D joint;
 
-    private AnchoredJoint2D Joint => joint ? joint : joint = GetComponent<AnchoredJoint2D>(); 
+    internal AnchoredJoint2D Joint => joint ? joint : joint = GetComponent<AnchoredJoint2D>(); 
     public bool HasResources() => Joint != null && Joint.connectedBody != null;
     public float zOffset = 0.1f;
     private void Update()
     {
         if (!HasResources())
         {
-            lr.enabled = false;
             return;
         }
 
