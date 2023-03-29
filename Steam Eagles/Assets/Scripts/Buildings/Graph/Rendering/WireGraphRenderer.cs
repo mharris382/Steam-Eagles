bd2b1f5ed40ade4e73a799984986cfc03ed9e987
Max <mharris382@gmail.com>
2023-03-29 16:43:26 -0400

    experimenting with wire graph rendering

---
+++ Steam Eagles/Assets/Scripts/Buildings/Graph/Rendering/WireGraphRenderer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Buildings.BuildingTilemaps;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace PhysicsFun.Buildings.Graph.Rendering
+{
+    public class WireGraphRenderer : MonoBehaviour
+    {
+        public Camera targetCamera;
+
+        public BuildingTilemap targetTilemap;
+
+        private void Awake()
+        {
+            var graph = new WireGraph(targetTilemap);
+            if (graph.VertexCount == 0)
+            {
+                Debug.LogError($"Cannot render wires because there are none on {targetTilemap.name}", this);
+                enabled = false;
+                return;
+            }
+            
+            int count = graph.GetStronglyConnectedComponents(out var components);
+            Debug.Log($"Found {count} strongly connected components in {targetTilemap.name}",this);
+        }
+    }
+}
--- Steam Eagles/Assets/Scripts/Buildings/Graph/TilemapGraph.cs
+++ Steam Eagles/Assets/Scripts/Buildings/Graph/TilemapGraph.cs
@@ -7,84 +7,99 @@ using QuikGraph;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using Debug = UnityEngine.Debug;
-
+using QuikGraph.Algorithms;
 namespace PhysicsFun.Buildings.Graph
 {
     public abstract class TilemapGraph
     {
-        private readonly AdjacencyGraph<Vector3Int,Edge<Vector3Int>> graph;
+        private AdjacencyGraph<Vector3Int,Edge<Vector3Int>> graph;
+        private readonly BuildingTilemap _tilemap;
+        
+        private int stronglyConnectedComponents = 0;
+        IDictionary<Vector3Int, int> scComponents = new Dictionary<Vector3Int, int>();
+        
+        public int VertexCount => graph.VertexCount;
+
+
+        public IEnumerable<Vector3Int> GetAllVertices() => graph.Vertices;
 
         public TilemapGraph(BuildingTilemap tilemap)
         {
+            void FindEdges(Dictionary<Vector3Int, TileBase> dictionary, Stopwatch timer1)
+            {
+                Vector3Int[] neighborDirections = new Vector3Int[4]
+                    { Vector3Int.right, Vector3Int.left, Vector3Int.up, Vector3Int.down };
+
+              
+                foreach (var nonEmptyTile in dictionary)
+                {
+                    foreach (var neighborDirection in neighborDirections)
+                    {
+                        var neighborPosition = nonEmptyTile.Key + neighborDirection;
+                        if (dictionary.ContainsKey(neighborPosition))
+                        {
+                            this.graph.AddEdge(new Edge<Vector3Int>(nonEmptyTile.Key, neighborPosition));
+                        }
+                    }
+                }
+
+                timer1.Stop();
+                Debug.Log(
+                    $"{tilemap.name}: Found {this.graph.EdgeCount} Edges in graph with {dictionary.Count} nodes non-empty tiles in {timer1.ElapsedMilliseconds} ms");
+            }
+            Dictionary<Vector3Int, TileBase> FindAllNonEmptyTiles(BoundsInt boundsInt, Tilemap tm1, Stopwatch stopwatch)
+            {
+                Dictionary<Vector3Int, TileBase> tileBases = new Dictionary<Vector3Int, TileBase>();
+                for (int x = boundsInt.xMin; x < boundsInt.xMax; x++)
+                {
+                    for (int y = boundsInt.yMin; y < boundsInt.yMax; y++)
+                    {
+                        for (int z = boundsInt.zMin; z < boundsInt.zMax; z++)
+                        {
+                            var position = new Vector3Int(x, y, z);
+                            var tile = tm1.GetTile(position);
+                            if (tile != null) tileBases.Add(position, tile);
+                        }
+                    }
+                }
+
+                Debug.Log($"{tilemap.name}: Found {tileBases.Count} non-empty tiles in {stopwatch.ElapsedMilliseconds} ms");
+                stopwatch.Reset();
+                return tileBases;
+            }
+
+            _tilemap = tilemap;
             var tm = tilemap.Tilemap;
             var bounds = tm.cellBounds;
-            
+
             var timer = new Stopwatch();
             timer.Start();
             
             //find all non-empty tiles
-            Dictionary<Vector3Int, TileBase> nonEmptyTiles = new Dictionary<Vector3Int, TileBase>();
-            for (int x = bounds.xMin; x < bounds.xMax; x++)
-            {
-                for (int y = bounds.yMin; y < bounds.yMax; y++)
-                {
-                    for (int z = bounds.zMin; z < bounds.zMax; z++)
-                    {
-                        var position = new Vector3Int(x, y, z);
-                        var tile = tm.GetTile(position);
-                        if (tile != null) nonEmptyTiles.Add(position, tile);
-                    }   
-                }
-            }
+            var nonEmptyTiles = FindAllNonEmptyTiles(bounds, tm, timer);
             
-            Debug.Log($"{tilemap.name}: Found {nonEmptyTiles.Count} non-empty tiles in {timer.ElapsedMilliseconds} ms");
-            timer.Reset();
-
-            Vector3Int[] neighborDirections = new Vector3Int[4] { Vector3Int.right, Vector3Int.left, Vector3Int.up, Vector3Int.down };
-
-#if false
-
-            //create graph by checking neighbors of each non-empty tile and adding edges
-            Dictionary<Vector3Int, Vector3Int[]> neighborDict = new Dictionary<Vector3Int, Vector3Int[]>();
-            List<Vector3Int> neighbors = new List<Vector3Int>();
-            int edgeCount = 0;
-            foreach (var nonEmptyTile in nonEmptyTiles)
-            {
-                neighbors.Clear();
-                foreach (var neighborDirection in neighborDirections)
-                {
-                    var neighborPosition = nonEmptyTile.Key + neighborDirection;
-                    if (nonEmptyTiles.ContainsKey(neighborPosition))
-                    {
-                        neighbors.Add(neighborPosition);
-                        edgeCount++;
-                    }
-                }
-                neighborDict.Add(nonEmptyTile.Key, neighbors.ToArray());
-            }
-
-            timer.Stop();
-            Debug.Log($"Found {edgeCount}({edgeCount/2}) Edges in graph with {nonEmptyTiles.Count} nodes non-empty tiles in {timer.ElapsedMilliseconds} ms");
             
-            //wrap dictionary in adjacency graph
-            this.graph = neighborDict.ToDelegateVertexAndEdgeListGraph(kv => Array.ConvertAll(kv.Value, v => new Edge<Vector3Int>(kv.Key, v)));
-            
-#endif
+            //init adjacency graph
             this.graph = new AdjacencyGraph<Vector3Int, Edge<Vector3Int>>();
             this.graph.AddVertexRange(nonEmptyTiles.Keys);
-            foreach (var nonEmptyTile in nonEmptyTiles)
-            {
-                foreach (var neighborDirection in neighborDirections)
-                {
-                    var neighborPosition = nonEmptyTile.Key + neighborDirection;
-                    if (nonEmptyTiles.ContainsKey(neighborPosition))
-                    {
-                        this.graph.AddEdge(new Edge<Vector3Int>(nonEmptyTile.Key, neighborPosition));
-                    }
-                }
-            }
-            timer.Stop();
-            Debug.Log($"{tilemap.name}: Found {this.graph.EdgeCount} Edges in graph with {nonEmptyTiles.Count} nodes non-empty tiles in {timer.ElapsedMilliseconds} ms");
+            
+            FindEdges(nonEmptyTiles, timer);
+        }
+
+
+        public int GetStronglyConnectedComponents(out IDictionary<Vector3Int, int> stronglyConnected)
+        {
+            scComponents = new Dictionary<Vector3Int, int>();
+            stronglyConnectedComponents = graph.StronglyConnectedComponents(scComponents);
+            stronglyConnected = scComponents;
+            return stronglyConnectedComponents;
+        }
+        void CountStronglyConnectedComponents()
+        {
+            IDictionary<Vector3Int, int> components = new Dictionary<Vector3Int, int>();
+            int componentCount = graph.StronglyConnectedComponents(components);
+            Debug.Log($"Graph of {_tilemap.name} has {componentCount} strongly connected components");
+            
         }
     }
 }
