4d60aae9b83393eb6021844f95233142df8e1c0d
Max <mharris382@gmail.com>
2023-04-04 09:45:26 -0400

    airship scene minor bugfixes

--- Steam Eagles/Assets/Scripts/SaveLoad/CoreSave/CoreSaveDataLoader.cs
+++ Steam Eagles/Assets/Scripts/SaveLoad/CoreSave/CoreSaveDataLoader.cs
@@ -1,10 +1,13 @@
-﻿namespace SaveLoad.CoreSave
+﻿using UnityEngine.SceneManagement;
+
+namespace SaveLoad.CoreSave
 {
     [LoadOrder(-1000)]
     public class CoreSaveDataLoader : SaveFileLoader<CoreSaveData>
     {
         public override bool LoadData(CoreSaveData data)
         {
+            SceneManager.LoadScene(data.Scene);
             throw new System.NotImplementedException();
         }
     }
