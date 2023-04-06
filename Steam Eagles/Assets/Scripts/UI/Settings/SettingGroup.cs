7c6559b43511df00b2a0677d162e56e7be777370
Max <mharris382@gmail.com>
2023-04-06 11:39:18 -0400

    updated settings menu in main menu

---
+++ Steam Eagles/Assets/Scripts/UI/Settings/SettingGroup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace UI.Settings
+{
+    public class SettingGroup : MonoBehaviour
+    {
+        public Subject<Unit> GroupOpened = new Subject<Unit>();
+        public Subject<Unit> GroupClosed = new Subject<Unit>();
+        public void OnEnable()
+        {
+            GroupOpened.OnNext(Unit.Default);
+        }
+
+        public void OnDisable()
+        {
+            GroupClosed.OnNext(Unit.Default);
+        }
+    }
+}
---
+++ Steam Eagles/Assets/Scripts/UI/Settings/SettingsGroupButton.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Settings
+{
+    [RequireComponent(typeof(Button))]
+    public class SettingsGroupButton : MonoBehaviour
+    {
+        [SerializeField,Required]
+        private SettingGroup groupPanel;
+        
+        [SerializeField,Required]
+        private Image panelOpenIcon;
+        
+        private Button _button;
+        
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+            panelOpenIcon.enabled = false;
+            
+            groupPanel.GroupOpened.Subscribe(_ =>
+            {
+                _button.interactable = false;
+                panelOpenIcon.enabled = true;
+            }).AddTo(this);
+            
+            groupPanel.GroupClosed.Subscribe(_ =>
+            {
+                _button.interactable = true;
+                panelOpenIcon.enabled = false;
+            }).AddTo(this);
+            
+            _button.onClick.AsObservable().Subscribe(_ =>
+            {
+                var panelParent = groupPanel.transform.parent;
+                for (int i = 0; i < panelParent.childCount; i++)
+                {
+                    var child = panelParent.GetChild(i);
+                    child.gameObject.SetActive(false);
+                }
+                groupPanel.gameObject.SetActive(true);
+            }).AddTo(this);
+        }
+    }
+}
