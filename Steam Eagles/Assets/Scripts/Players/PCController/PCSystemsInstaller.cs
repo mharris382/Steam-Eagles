5c6da2ff0b16820723c75343c368ed83540d1cda
Max <mharris382@gmail.com>
2023-05-23 19:01:47 -0400

    Update PCSystemsInstaller.cs

--- Steam Eagles/Assets/Scripts/Players/PCController/PCSystemsInstaller.cs
+++ Steam Eagles/Assets/Scripts/Players/PCController/PCSystemsInstaller.cs
@@ -2,16 +2,40 @@ using Buildings.Rooms.Tracking;
 using CoreLib;
 using FSM;
 using Players.PCController;
+using UnityEngine;
 using Zenject;
 
 public class PCSystemsInstaller : MonoInstaller
 {
+    public bool enableDebugging = true;
     public override void InstallBindings()
     {
         Container.BindInterfacesAndSelfTo<PCManager>().AsSingle().NonLazy();
         Container.BindFactory<int, PCTracker.TrackedPC, PC, PC.Factory>();
         Container.BindInterfacesAndSelfTo<PCTracker>().FromNew().AsSingle().NonLazy();
-        
+        Container.BindInterfacesAndSelfTo<PCEventPublisher>().FromNew().AsSingle().NonLazy();
+
+        if (enableDebugging)
+        {
+            Container.BindInterfacesAndSelfTo<PCEventDebugger>().AsSingle().NonLazy();
+            Debug.Log("Enabled PC Event Debugger", this);
+        }
+        Container.Bind<GlobalPCInfo>().AsSingle().NonLazy();
     }
+
     
+    
+    public class PCEventDebugger : IInitializable
+    {
+        private readonly GlobalPCInfo _pcInfo;
+
+        public PCEventDebugger(GlobalPCInfo pcInfo)
+        {
+            _pcInfo = pcInfo;
+        }
+        public void Initialize()
+        {
+            
+        }
+    }
 }
