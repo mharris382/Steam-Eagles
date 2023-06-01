339fc8599dab7b02b03b5ea33b091e1fd1adcff5
Max <mharris382@gmail.com>
2023-05-31 22:50:06 -0400

    created steam graph file

---
+++ Steam Eagles/Assets/Scripts/Power/PowerTests/SteamGraph.cs
@@ -0,0 +1,19 @@
+﻿
+
+using UnityEngine;
+
+public class SteamNetwork
+{
+    
+}
+
+
+public class SteamSupplier: GridNode
+{
+    public SteamSupplier(Vector3Int position) : base(position) {}
+}
+
+public class SteamConsumer : GridNode
+{
+    public SteamConsumer(Vector3Int position) : base(position) { }
+}
